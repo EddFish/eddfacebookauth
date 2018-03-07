0ec19f2e77eda9b1d2631d7414ff79306ea05de8
edward <edward@EdwardLFish>
2018-03-08 01:20:16 +0200

    Added config

--- FaceBookTest/src/main/java/telran/facebook/FaceBookTestApplication.java
+++ FaceBookTest/src/main/java/telran/facebook/FaceBookTestApplication.java
@@ -5,6 +5,8 @@ import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableOAuth2Sso
 @SpringBootApplication
 @CrossOrigin
-public class FaceBookTestApplication {
+public class FaceBookTestApplication extends WebSecurityConfigurerAdapter{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FaceBookTestApplication.class, args);
 	}
 	
-	@GetMapping("/")
-	public String home(Principal user) {
+	  @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	      http
+	        .csrf().disable()
+	        .antMatcher("/**")
+	        .authorizeRequests()
+	          .antMatchers("/", "/login**", "/webjars/**")
+	          .permitAll()
+	        .anyRequest()
+	          .authenticated()
+	        .and().logout().logoutSuccessUrl("/").permitAll();
+	  }
+	
+	@GetMapping("/user")
+	public String user(Principal user) {
 		
 		return "Hello " + user.getName();
 	}
